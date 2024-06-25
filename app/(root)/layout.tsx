0bf9c725985332acecf1c3994b8bc7a2fb3fa8b0
WP Kindle <abdulkhaliqwebdeveloper@gmail.com>
2024-06-25 09:09:15 +0500

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/components/shrared/footer";
+import Header from "@/components/shrared/header";
+import React from "react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1 wrapper">{children}</main>
+      <Footer />
+    </div>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import ProductList from "@/components/shrared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+
+export default async function Home() {
+  const latestProducts = await getLatestProducts();
+  return (
+    <div className="space-y-8">
+      <ProductList title="Newest Arrivals" data={latestProducts} />
+    </div>
+  );
+}
