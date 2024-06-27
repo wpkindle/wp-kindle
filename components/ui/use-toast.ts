6ab6ad9e1c15d0baf3f71798afb6d1a5167cb61f
WP Kindle <abdulkhaliqwebdeveloper@gmail.com>
2024-06-27 12:25:00 +0500

    first commit

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -169,7 +169,7 @@ function toast({ ...props }: Toast) {
 }
 
 function useToast() {
-  const [state, setState] = React.useState<State>({ toasts: [] });
+  const [state, setState] = React.useState<State>(memoryState);
 
   React.useEffect(() => {
     listeners.push(setState);
