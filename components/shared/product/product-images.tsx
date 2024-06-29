454667aea2ffe8b019c55230eb63f010de8cfa79
WP Kindle <abdulkhaliqwebdeveloper@gmail.com>
2024-06-29 12:50:27 +0500

    first commit

--- components/shared/product/product-images.tsx
+++ components/shared/product/product-images.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 // import { cn } from "@/lib/utils";
 
 export default function ProductImages({ images }: { images: string[] }) {
-  const [current, setCurrent] = React.useState(0);
+  const [current] = React.useState(0);
 
   return (
     <div className="space-y-4">
