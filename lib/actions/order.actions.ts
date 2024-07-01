5cd30ab0d576b67a17bfc886fb06d99cc47f9371
WP Kindle <abdulkhaliqwebdeveloper@gmail.com>
2024-07-01 06:40:41 +0500

    first commit

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -10,7 +10,6 @@ import { carts, orderItems, orders, products, users } from "@/db/schema";
 import { count, desc, eq, sql, sum } from "drizzle-orm";
 import { isRedirectError } from "next/dist/client/components/redirect";
 import { formatError } from "../utils";
-import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
 import { PaymentResult } from "@/types";
 import { PAGE_SIZE } from "../constants";
@@ -174,37 +173,6 @@ export async function deleteOrder(id: string) {
 }
 
 // UPDATE
-export async function createPayPalOrder(orderId: string) {
-  try {
-    const order = await db.query.orders.findFirst({
-      where: eq(orders.id, orderId),
-    });
-    if (order) {
-      const paypalOrder = await paypal.createOrder(Number(order.totalPrice));
-      await db
-        .update(orders)
-        .set({
-          paymentResult: {
-            id: paypalOrder.id,
-            email_address: "",
-            status: "",
-            pricePaid: "0",
-          },
-        })
-        .where(eq(orders.id, orderId));
-      return {
-        success: true,
-        message: "PayPal order created successfully",
-        data: paypalOrder.id,
-      };
-    } else {
-      throw new Error("Order not found");
-    }
-  } catch (err) {
-    return { success: false, message: formatError(err) };
-  }
-}
-
 export async function createPayMobOrder(
   orderId: string,
   orderItems: any,
@@ -243,43 +211,6 @@ export async function createPayMobOrder(
     } else {
       throw new Error("Order not found");
     }
-  } catch (err) {
-    return { success: false, message: formatError(err) };
-  }
-}
-
-export async function approvePayPalOrder(
-  orderId: string,
-  data: { orderID: string }
-) {
-  try {
-    const order = await db.query.orders.findFirst({
-      where: eq(orders.id, orderId),
-    });
-    if (!order) throw new Error("Order not found");
-
-    const captureData = await paypal.capturePayment(data.orderID);
-    if (
-      !captureData ||
-      captureData.id !== order.paymentResult?.id ||
-      captureData.status !== "COMPLETED"
-    )
-      throw new Error("Error in paypal payment");
-    await updateOrderToPaid({
-      orderId,
-      paymentResult: {
-        id: captureData.id,
-        status: captureData.status,
-        email_address: captureData.payer.email_address,
-        pricePaid:
-          captureData.purchase_units[0]?.payments?.captures[0]?.amount?.value,
-      },
-    });
-    revalidatePath(`/order/${orderId}`);
-    return {
-      success: true,
-      message: "Your order has been successfully paid by PayPal",
-    };
   } catch (err) {
     return { success: false, message: formatError(err) };
   }
